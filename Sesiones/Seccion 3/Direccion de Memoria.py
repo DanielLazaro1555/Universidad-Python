8f085a0de352de75965ee31570e2702d1d542b4c
danielhuaman <danielsam_m@hotmail.com>
2024-01-04 00:35:52 -0500

    Nuevo aprendizaje

---
+++ Sesiones/Seccion 3/Direccion de Memoria.py
@@ -0,0 +1,9 @@
+# Direccion de Memoria y Variables en Python
+x = 10
+y = 2
+z = x + y
+
+print(x)
+print(y)
+print(z)
+print(id(x))
---
+++ Sesiones/Seccion 3/Variables2.py
@@ -0,0 +1,8 @@
+# Realizando con otras variables
+
+x = 10
+y = 2
+z = x + y
+print(x)
+print(y)
+print(z)
