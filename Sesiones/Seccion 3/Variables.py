9358d893a4da5028cd1f9a17031690c7f86bfbf7
danielhuaman <danielsam_m@hotmail.com>
2024-01-04 00:26:12 -0500

    Variables

---
+++ Sesiones/Seccion 3/Variables.py
@@ -0,0 +1,13 @@
+#Llamando desde las variables
+mivariable = "Hola desde Python";
+print (mivariable)
+
+#Cambiando la variable
+
+mivariable = "2";
+print (mivariable)
+
+#Cambiando a variable 10
+
+mivariable = "10";
+print (mivariable)
