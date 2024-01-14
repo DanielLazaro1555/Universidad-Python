9c744052cee6da03242489cbc50128318be65b12
danielhuaman <danielsam_m@hotmail.com>
2024-01-14 01:36:53 -0500

    Nuevos cambios

---
+++ Sesiones/Seccion 4/Operadores de comparacion.py
@@ -0,0 +1,22 @@
+a = 4
+b = 2
+# Operador de asignacion
+resultado = (4 == 2)
+print(f'Resultado == :{resultado}')
+
+resultado = a != b
+print(f'Resultado == :{resultado}')
+
+
+resultado = a > b
+print(f'Resultado > : {resultado}')
+
+
+resultado = a >= b
+print(f'Resultado >= : {resultado}')
+
+resultado = a < b
+print(f'Resultado < : {resultado}')
+
+resultado = a <= b
+print(f'Resultado : <= : {resultado}')
