aed38644e8c0b03b1bc1f38880acec08cfdcd32f
danielhuaman <danielsam_m@hotmail.com>
2024-01-14 01:07:12 -0500

    Nuevos datos

---
+++ Sesiones/Seccion 4/OperadoresdeAsignacion.py
@@ -0,0 +1,18 @@
+# Operacion clasica de asignacion
+miVariable = 10
+print(miVariable)
+# Operacion de adicion a la asignacion
+miVariable = miVariable + 1
+print(miVariable)
+# Incremento con reasignacion
+miVariable += 1
+print(miVariable)
+# Decremento con reasignacion
+miVariable -= 2
+print(miVariable)
+# Multiplicacion con reasignacion
+miVariable *= 3
+print(miVariable)
+# Division con reasignacion
+miVariable /= 2
+print(miVariable)
