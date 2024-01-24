938b3a98c9501d485e9c5cbbcd7dbb866b3264b1
danielhuaman <danielsam_m@hotmail.com>
2024-01-24 09:32:15 +0100

    Nuevo dato

---
+++ Sesiones/Seccion 4/EjercicioDeterminaEdad.py
@@ -0,0 +1,21 @@
+from builtins import ValueError
+
+try:
+    a = int(input('Introduzca la edad:'))
+    if a > 18:
+        print(f'Es mayor de edad')
+    else:
+        print(f'Es menor de edad')
+except ValueError:
+    print('El valor ingresado no es válido.')
+
+
+#Ejercicio resuelto en clase
+
+edadAdulto = 18
+
+S80099
+Clara SANCHEZ LAROSA
+S80099
+P07131000CCV154
+LLS01267
