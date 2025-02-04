b0f84d750e111ba99fab0dccec9e614ff10f1947
danielhuaman <danielsam_m@hotmail.com>
2025-02-04 00:39:50 -0500

    Nuevos datos
    Ejercicios Resueltos en Python de la sesión 2

---
+++ Sesiones/Seccion 3 : Variables en Python/Variables.py
@@ -0,0 +1,30 @@
+# Ejemplos de uso de variables en Python
+
+# Llamando desde las variables
+mivariable = "Hola desde Python"
+print(mivariable)
+
+# Cambiando el valor de la variable
+mivariable = 2  # Ahora es un número entero
+print(mivariable)
+
+# Cambiando a otro valor numérico
+mivariable = 10  # Valor entero
+print(mivariable)
+
+#####################################################
+
+# Sintaxis para definir una variable
+nombre_de_la_variable = "valor de la variable"
+
+# Declaración de variables y asignación de valores
+nombre = "Maria"       # Cadena de texto (str)
+edad = 30              # Número entero (int)
+peso = 65.5            # Número decimal o de punto flotante (float)
+es_casado = False      # Valor booleano (bool)
+
+# Mostrando las variables en consola
+print("Nombre:", nombre)
+print("Edad:", edad)
+print("Peso:", peso)
+print("¿Está casado?:", es_casado)
