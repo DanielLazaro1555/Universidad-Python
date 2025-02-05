d2b534e7ed0de37821f8f3169606620e52733960
danielhuaman <danielsam_m@hotmail.com>
2025-02-05 14:15:08 -0500

    Update Variables.py

--- Sesiones/Seccion 3 : Variables en Python/Variables.py
+++ Sesiones/Seccion 3 : Variables en Python/Variables.py
@@ -28,3 +28,37 @@ print("Nombre:", nombre)
 print("Edad:", edad)
 print("Peso:", peso)
 print("¿Está casado?:", es_casado)
+
+#######################################################
+
+# Variables en python
+
+# Declaración e inicialización de variables
+
+edad = 28  # Valor tipo Entero
+altura = 1.65  # Valor tipo Flotante
+pais = 'Mexico'  # Valor tipo Cadena
+
+# Acceder a las variables
+print('Edad:', edad)
+print('Altura:', altura)
+print('País:', pais)
+
+# Modificar el valor de una variable
+edad = 30
+altura = 1.68
+
+# Acceder a las variables
+print('Valores modificados:')
+print('Edad:', edad)
+print('Altura:', altura)
+print('País:', pais)
+
+# En python el tipo es dinámico
+
+edad = 'Treinta'
+print('Edad:', edad)
+
+# Si queremos acceder a una variable no declarada manda error
+telefono = '123456789'
+print('telefono:', telefono)
